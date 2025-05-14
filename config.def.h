004df8dffeed61fa3b0879c89ac1595d22633b31
Rafael Marçalo <public@rafaelmarcalo.xyz>
2025-05-14 20:16:55 +0100

    Revert "Play external patch"
    This reverts commit d7eab80b770056d648f90c33ecf45ec06b6617c1.

--- config.def.h
+++ config.def.h
@@ -172,8 +172,6 @@ static Key keys[] = {
 	{ MODKEY,                GDK_KEY_m,      spawn,      BM_ADD("_SURF_URI") },
 	{ MODKEY,                GDK_KEY_s,      spawn,      SEARCH() },
 
-	{ MODKEY,                GDK_KEY_w,      playexternal, { 0 } },
-
 	{ 0,                     GDK_KEY_Escape, stop,       { 0 } },
 	{ MODKEY,                GDK_KEY_c,      stop,       { 0 } },
 
--- surf.c
+++ surf.c
@@ -238,7 +238,6 @@ static void togglecookiepolicy(Client *c, const Arg *a);
 static void toggleinspector(Client *c, const Arg *a);
 static void find(Client *c, const Arg *a);
 static void search(Client *c, const Arg *a);
-static void playexternal(Client *c, const Arg *a);
 
 /* Buttons */
 static void clicknavigate(Client *c, const Arg *a, WebKitHitTestResult *h);
@@ -2012,15 +2011,6 @@ clickexternplayer(Client *c, const Arg *a, WebKitHitTestResult *h)
 	Arg arg;
 
 	arg = (Arg)VIDEOPLAY(webkit_hit_test_result_get_media_uri(h));
-	spawn(c, &arg);
-}
-
-void
-playexternal(Client *c, const Arg *a)
-{
-	Arg arg;
-
-	arg = (Arg)VIDEOPLAY(geturi(c));
 	spawn(c, &arg);
 }
 
